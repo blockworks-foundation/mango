e949352888ed9446bd8e21bd8898094b50ce4efe
dd <dafydd.durairaj@gmail.com>
2020-12-07 11:28:54 -0500

    Added init_mango instruction utility

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -1,8 +1,10 @@
 use arrayref::{array_ref, array_refs};
 use serde::{Deserialize, Serialize};
-use solana_program::instruction::Instruction;
+use solana_program::instruction::{Instruction, AccountMeta};
 use solana_program::program_error::ProgramError;
 use solana_program::pubkey::Pubkey;
+use crate::state::{NUM_TOKENS, NUM_MARKETS};
+
 
 #[repr(C)]
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -12,7 +14,21 @@ pub enum MangoInstruction {
     /// Accounts expected by this instruction:
     ///
     /// 0. `[writable]` mango_group_acc - the data account to store mango group state vars
-    /// 1.
+    /// 1. `[]` rent_acc - Rent sysvar account
+    /// 2. `[]` clock_acc - clock sysvar account
+    /// 3. `[]` signer_acc - pubkey of program_id hashed with signer_nonce and mango_group_acc.key
+    /// 4. `[]` dex_prog_acc - program id of serum dex
+    /// 5..5+NUM_TOKENS `[]` token_mint_accs - mint of each token in the same order as the spot
+    ///     markets. Quote currency mint should be last.
+    ///     e.g. for spot markets BTC/USDC, ETH/USDC -> [BTC, ETH, USDC]
+    ///
+    /// 5+NUM_TOKENS..5+2*NUM_TOKENS `[writable]`
+    ///     vault_accs - Vault owned by signer_acc.key for each of the mints
+    ///
+    /// 5+2*NUM_TOKENS..5+2*NUM_TOKENS+NUM_MARKETS `[]`
+    ///     spot_market_accs - MarketState account from serum dex for each of the spot markets
+    ///
+    /// Total number of accounts = 5 + 2 * NUM_TOKENS + NUM_MARKETS
     InitMangoGroup {
         signer_nonce: u64
     },
@@ -52,12 +68,29 @@ impl MangoInstruction {
     }
 }
 
+
 pub fn init_mango_group(
     program_id: &Pubkey,
+    mango_group_pk: &Pubkey,
+    signer_pk: &Pubkey,
+    dex_prog_id: &Pubkey,
+    mint_pks: &[Pubkey; NUM_TOKENS],
+    vault_pks: &[Pubkey; NUM_TOKENS],
+    spot_market_pks: &[Pubkey; NUM_MARKETS],
     signer_nonce: u64,
 ) -> Result<Instruction, ProgramError> {
     let instr = MangoInstruction::InitMangoGroup { signer_nonce };
-    let accounts = vec![];
+    let mut accounts = vec![
+        AccountMeta::new(*mango_group_pk, false),
+        AccountMeta::new_readonly(solana_program::sysvar::rent::ID, false),
+        AccountMeta::new_readonly(solana_program::sysvar::clock::ID, false),
+        AccountMeta::new_readonly(*signer_pk, false),
+        AccountMeta::new_readonly(*dex_prog_id, false)
+    ];
+    accounts.extend(mint_pks.iter().map(|pk| AccountMeta::new_readonly(*pk, false)));
+    accounts.extend(vault_pks.iter().map(|pk| AccountMeta::new(*pk, false)));
+    accounts.extend(spot_market_pks.iter().map(|pk| AccountMeta::new_readonly(*pk, false)));
+
     let data = instr.pack();
     Ok(Instruction {
         program_id: *program_id,
