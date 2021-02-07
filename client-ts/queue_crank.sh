ba62e4024fc899a34596f0d67e2a7035cf824429
Maximilian Schneider <mail@maximilianschneider.net>
2021-02-07 01:56:27 +0100

    moved client-ts to its own repo

--- client-ts/queue_crank.sh
+++
@@ -1,3 +0,0 @@
-cd ~/serum-dex/dex/crank
-source ~/mango/client-ts/devnet.env
-cargo run -- $CLUSTER consume-events --dex-program-id $DEX_PROGRAM_ID --payer $KEYPAIR --market $BTCUSD --coin-wallet $BTC_WALLET --pc-wallet $USDC_WALLET --num-workers 1 --events-per-worker 5 --log-directory .
