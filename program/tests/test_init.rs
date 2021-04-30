dda3b1e988d9bfa1795ac3ae316dedeae11b11ab
Maximilian Schneider <mail@maximilianschneider.net>
2021-04-30 10:54:40 +0300

    add test to create mango group without dedicated SRM fee vault

--- program/tests/test_init.rs
+++ program/tests/test_init.rs
@@ -18,7 +18,7 @@ use mango::state::MangoGroup;
 use common::create_signer_key_and_nonce;
 
 #[tokio::test]
-async fn test_init() {
+async fn test_init_btc_eth_usdt() {
     let program_id = Pubkey::new_unique();
 
     let mut test = ProgramTest::new(
@@ -56,6 +56,77 @@ async fn test_init() {
     let btc_usdt = add_aggregator(&mut test, "BTC:USDT", 6, 50_000 * unit, &program_id);
     let eth_usdt = add_aggregator(&mut test, "ETH:USDT", 6, 2_000 * unit, &program_id);
     let oracles = vec![btc_usdt.pubkey, eth_usdt.pubkey];
+
+    let (mut banks_client, payer, recent_blockhash) = test.start().await;
+
+    let borrow_limits = [100, 100, 100];
+
+    let mut transaction = Transaction::new_with_payer(
+        &[
+            init_mango_group(
+                &program_id,
+                &mango_group_pk,
+                &signer_pk,
+                &dex_prog_id,
+                &srm_vault.pubkey,
+                &payer.pubkey(),
+                &mints,
+                &vaults,
+                &dexes,
+                &oracles,
+                signer_nonce,
+                U64F64::from_num(1.1),
+                U64F64::from_num(1.2),
+                borrow_limits,
+            ).unwrap(),
+        ],
+        Some(&payer.pubkey()),
+    );
+
+    transaction.sign(
+        &[&payer],
+        recent_blockhash,
+    );
+    assert!(banks_client.process_transaction(transaction).await.is_ok());
+}
+
+
+#[tokio::test]
+async fn test_init_srm_sol_usdt() {
+    let program_id = Pubkey::new_unique();
+
+    let mut test = ProgramTest::new(
+        "mango",
+        program_id,
+        processor!(process_instruction),
+    );
+
+    // limit to track compute unit increase
+    test.set_bpf_compute_max_units(51_000);
+
+    let mango_group_pk = Pubkey::new_unique();
+    let (signer_pk, signer_nonce) = create_signer_key_and_nonce(&program_id, &mango_group_pk);
+    test.add_account(mango_group_pk, Account::new(u32::MAX as u64, size_of::<MangoGroup>(), &program_id));
+
+    let sol_mint = add_mint(&mut test, 6);
+    let srm_mint = add_mint_srm(&mut test);
+    let usdt_mint = add_mint(&mut test, 6);
+    let mints = vec![sol_mint.pubkey, srm_mint.pubkey, usdt_mint.pubkey];
+
+    let sol_vault = add_token_account(&mut test, signer_pk, sol_mint.pubkey);
+    let srm_vault = add_token_account(&mut test, signer_pk, srm_mint.pubkey);
+    let usdt_vault = add_token_account(&mut test, signer_pk, usdt_mint.pubkey);
+    let vaults = vec![sol_vault.pubkey, srm_vault.pubkey, usdt_vault.pubkey];
+
+    let dex_prog_id = Pubkey::new_unique();
+    let sol_usdt_dex = add_dex_empty(&mut test, sol_mint.pubkey, usdt_mint.pubkey, dex_prog_id);
+    let srm_usdt_dex = add_dex_empty(&mut test, srm_mint.pubkey, usdt_mint.pubkey, dex_prog_id);
+    let dexes = vec![sol_usdt_dex.pubkey, srm_usdt_dex.pubkey];
+
+    let unit = 10u64.pow(6);
+    let sol_usdt = add_aggregator(&mut test, "SOL:USDT", 6, 50_000 * unit, &program_id);
+    let srm_usdt = add_aggregator(&mut test, "SRM:USDT", 6, 2_000 * unit, &program_id);
+    let oracles = vec![sol_usdt.pubkey, srm_usdt.pubkey];
 
     let (mut banks_client, payer, recent_blockhash) = test.start().await;
 
